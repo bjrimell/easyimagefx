d1f317a87ebb4a1864786cc9535e87a839bc9e16
bjrimell <barryrimell@gmail.com>
2016-04-25 12:57:33 +0100

    Initial file commit
    Basic PHP file for wordpress plugin, comments and file extension only.

---
+++ easy-image-fx.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Easy Image FX
+Plugin URI: http://www.tortan-limited.co.uk/easyimagefx
+Version: 0.1
+Author: Tortan Limited
+Description: Want a simple, easy to use plugin that will let you apply effects on images when the user hovers over the image? Look no further.
+Easy Image Hover FX is a SIMPLE and EASY to use plugin that will let you apply effects upon mouseover/hover. This early version includes just one feature:
+	- Image Replace - when a user hovers over an image, that image will be replaced with another of your choice.
+
+In the future, further effects will be added.
+*/
+?>
